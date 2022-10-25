87f14cbc91e83c34995e45d879f2d52a24c0adc5
Thomas Auinger <thomas@auinger.de>
2022-10-25 19:41:33 +0200

    fixed StudentListServlet

--- src/main/java/de/auinger/training/spring_boot/StudentListServlet.java
+++ src/main/java/de/auinger/training/spring_boot/StudentListServlet.java
@@ -3,34 +3,27 @@ package de.auinger.training.spring_boot;
 import de.auinger.training.spring_boot.student.Student;
 import de.auinger.training.spring_boot.student.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(value = "/students")
-public class AnnotatedServlet extends HttpServlet {
+public class StudentListServlet extends HttpServlet {
 
     @Autowired
-    StudentService studentService;
-
-    public AnnotatedServlet() {
-        System.out.println("Constructing AnnotatedServlet");
-    }
+    private StudentService studentService;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         var students = studentService.getStudents();
-        System.out.println(getClass().getSimpleName() + ": doGet()");
 
         response.setContentType("text/plain");
         for (Student student : students) {
@@ -43,8 +36,7 @@ public class AnnotatedServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init();
         var servletContext = getServletContext();
-        AutowireCapableBeanFactory beanFactory = ((ApplicationContext) servletContext.getAttribute(
-                "applicationContext")).getAutowireCapableBeanFactory();
-        beanFactory.autowireBean(this);
+        WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+        ctx.getAutowireCapableBeanFactory().autowireBean(this);
     }
 }
